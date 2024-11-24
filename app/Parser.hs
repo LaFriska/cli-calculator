8daf7ac2e8e58e69500f07b85d964fc5ca768945
LaFriska <harold.gao205@gmail.com>
2024-11-24 15:28:17 +1100

    started work on getSubequation

--- app/Parser.hs
+++ app/Parser.hs
@@ -10,6 +10,9 @@ instance Show Operator where
 
 data Equation = Num Float 
               | Node Operator Equation Equation
+
+data EquationErr = Eq Equation | Err String 
+data StringErr = Str String | Err String
               
 instance Show Equation where 
     show eq = case eq of 
@@ -20,3 +23,16 @@ instance Show Equation where
         wrap subeq = case subeq of 
             Num _      -> show subeq
             Node _ _ _ -> "(" ++ (show subeq) ++ ")"
+
+parse :: String -> EquationErr
+parse string = undefined
+
+-- Returns a Maybe EquationErr surrounded by an outher layer of brackets, or error
+-- if there is a bracket mismatch, or Nothing if no brackets are found. 
+getSubequation :: String -> Int -> Bool -> Maybe StringErr -> 
+getSubequation str bracketCount hasBracket currentSub = case (bracketCount, hasBracket) of --TODO
+    (0, True)  -> Just (Str currentSub)
+    (0, False) -> case str of
+        []   -> Nothing 
+        x:xs -> undefined 
+    (_, True)  -> undefined 
